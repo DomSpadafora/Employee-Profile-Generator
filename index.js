ab105788b675c92e5a3e3741b516f71c528ea7aa
DomSpadafora <120040191+DomSpadafora@users.noreply.github.com>
2023-05-01 11:41:30 -0400

    require dependancies

---
+++ index.js
@@ -0,0 +1,9 @@
+//packages/classes needed for application
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
+const Manager = require('./lib/Manager');
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern')
+
